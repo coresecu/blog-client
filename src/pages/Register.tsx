fb900076de244fc9461c3fef62c96ab0c861b7de
coresecu <coresecu@proton.me>
2023-08-10 21:14:41 +0900

    Update Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const Register = () => {
   return (
     <div className="auth">
-      <h1>Login</h1>
+      <h1>Register</h1>
       <form>
         <input required type="text" name="" id="" placeholder='username'/>
         <input required type="email" name="" id="" placeholder='email'/>
         <input required type="password" name="" id="" placeholder='password'/>
         <button>Login</button>
         <p>This is an error.</p>
-        <span>Do you have an account? <Link to='/login'>Login</Link></span>
+        <span>Do you have an account? <Link to='/login'>Register</Link></span>
       </form>
     </div>
   )
